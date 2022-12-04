bc4c383026f45e33c7bb199890ec0c37b8fbd552
Gonzalo Gil Paricio <gonzalo.gilparicio@gmail.com>
2022-12-04 03:33:14 -0300

    add logical AND & object destructuring

--- js/busqueda.js
+++ js/busqueda.js
@@ -32,21 +32,23 @@ formularioBusqueda.onsubmit = (e) => {
 //si ultimaBusqueda está en el localStorage, lo sigue renderizando
 
 const keyValue = localStorage.getItem('ultimaBusqueda');
-if (keyValue !== null) {
-    ultimaBusqueda.innerHTML = '<span>Ultima búsqueda realizada: ' + keyValue + '</span>';
-}
+keyValue !== null && (ultimaBusqueda.innerHTML = '<span>Ultima búsqueda realizada: ' + keyValue + '</span>');
+    
+
 
 //funcion que hace busqueda de GPUs sobre el array, manipula dom, mete y recupera localStorage
 //y tambien convierte obj a json y viceversa
 
-function busquedaGpu(e, x) {
-    const resultado = productos.find(el => el.name === e);
+function busquedaGpu(e, x) {    
+    const resultado = productos.find(el => el.name === e);    
     localStorage.setItem('ultimaBusqueda', x)
     if (resultado == undefined) {
         resultadoBusqueda.innerHTML = '<p>No tenemos esa GPU en stock en este momento</p>';
         localStorage.setItem('objetoEncontrado', 'Sin coincidencias');
+        inputTextBusqueda.value = '';
     } else {
-        resultadoBusqueda.innerHTML = '<p>Tenemos ' + resultado.stock + ' unidades en stock de la ' + resultado.name + ' con un valor de $' + resultado.price + '</p>';
+        const {stock,name,price} = resultado;
+        resultadoBusqueda.innerHTML = '<p>Tenemos ' + stock + ' unidades en stock de la ' + name + ' con un valor de $' + price + '</p>';
         const resultadoObjetoJson = JSON.stringify(resultado);
         localStorage.setItem('objetoEncontrado', resultadoObjetoJson);
         const infoStorageObjJson = JSON.parse(localStorage.getItem('objetoEncontrado'));
